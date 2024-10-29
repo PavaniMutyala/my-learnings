980bddb104f3792a81bb27073b8035646d3fcc58
PavaniMutyala <pavanimutyala44@gmail.com>
2024-10-29 14:13:32 +0530

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,14 @@
+ class Arithmetic {
+    public static void main(String[] args){
+        int a=10;
+    int b=20;
+    System.out.println("Add of two numbers: "+(a+b));
+    System.out.println("Sub of two numbers: "+(a-b));
+    System.out.println("Mult of two numbers: "+(a*b));
+    System.out.println("div of two numbers: "+(a/b));
+
+    }
+    
+   
+   
+}
